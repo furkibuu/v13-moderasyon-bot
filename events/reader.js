b6878536571c2662e71bc18427c20992d45b5e9c
fu-w <74312970+fu-w@users.noreply.github.com>
2021-05-27 15:31:39 +0300

    Add files via upload

---
+++ events/reader.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} İsmi İle Giriş Yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT:`);
+  client.user.setStatus("idle");//dnd : Rahatsız Etmeyin //idle : Boşta //online : Çevrimiçi
+ client.user.setActivity("Sanctus İYİ EĞLENCELER DİLER", { type: "LISTENING" }); ///PLAYING : Oynuyor //WATCING : İzliyor //LISTENING : Dinliyor
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+ console.log(`Sanctus Tarafından Yapıldı İyi Kullanımlar`)
+};
---
+++ komutlar/ban.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+exports.run = (client, message, args) => {
+  
+
+    
+  if (!message.guild.members.get(client.user.id).hasPermission("BAN_MEMBERS")) return message.reply('Gerekli izin yok')
+  if (!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`Bu komutu kullanabilmek için **Üyeleri Yasakla** iznine sahip olmalısın!`);
+  
+  let user = message.mentions.users.first();
+  let reason = args.slice(1).join(' ');
+  //let modLog = JSON.parse(fs.readFileSync("./jsonlar/mLog.json", "utf8"));
+  if (message.mentions.users.size < 1) return message.reply('Lütfen Banlamak İstediğiniz Üyeyi Etiketleyin Veya Id sını Yazın');
+  if (reason.length < 1) return message.reply('Lütfen sebep giriniz');
+  if (user.id === message.author.id) return message.reply('Kendinimi banlayacaksın?');
+  if (user.highestRole.calculatedPosition > message.member.highestRole.calculatedPosition - 1) {
+			return message.reply(`Bu kişinin senin rollerinden/rolünden daha yüksek rolleri/rolü var.`);
+		}
+  if (!message.guild.member(user).bannable) return message.channel.send(`Bu kişiyi sunucudan yasaklayamıyorum çünkü \`benden daha yüksek bir role sahip\` ya da \`bana gerekli yetkileri vermedin\`.`);
+
+  
+   if (!message.guild.member(user).bannable) return message.reply('Yetkilileri yasaklayamam!');
+  message.guild.ban(user, 2);
+  
+  const embed2 = new Discord.MessageEmbed()
+  .setColor("BLACK")
+  .setDescription(`:x: Başarıyla banlandı`)
+  message.channel.send(embed2)
+    
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['ban'],
+  permLevel: 3,
+    kategori: "moderasyon",
+};
+
+exports.help = {
+  name: 'yasakla',
+  description: 'İstediğiniz kişiyi sunucudan yasaklar.',
+  usage: 'yasakla <@kullanıcı> <sebep>',
+ 
+};
