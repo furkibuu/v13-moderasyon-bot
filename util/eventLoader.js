f113ee5d5afc921b63750020e3001c156e1fb5ee
Sanctus Furkan <74312970+fu-w@users.noreply.github.com>
2021-07-16 00:42:31 +0300

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,5 +1,6 @@
-const reqEvent = (event) => require(`../events/${event}`);
+const reqEvent = (event) => require(`../events/${event}`);//sanctus
 module.exports = client => {
-  client.on('ready', () => reqEvent('ready')(client));
+client.on('ready', () => reqEvent('ready')(client));
   client.on('message', reqEvent('message'));
 };
+//sanctus
