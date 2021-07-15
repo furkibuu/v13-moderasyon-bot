b5fea56bc7d8a332016fad19a031b4aa3dc66a7b
Sanctus Furkan <74312970+fu-w@users.noreply.github.com>
2021-07-16 00:32:58 +0300

    Update sanctus.js

--- sanctus.js
+++ sanctus.js
@@ -4,78 +4,403 @@ const fs = require("fs");
 const moment = require("moment");
 const chalk = require("chalk");
 const ayarlar = require("./ayarlar.json");
-require('./util/eventLoader')(client); 
+var prefix = ayarlar.prefix;
 
-var önlek = ayarlar.önlek;
+require('./util/eventLoader')(client);
+
 
 
 const log = message => {
-    console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-  };
-  
-  client.commands = new Discord.Collection();
-  client.aliases = new Discord.Collection();
-  fs.readdir("./komutlar/", (err, files) => {
-    if (err) console.error(err);
-    log(`${files.length} komut yüklenecek.`);
-    files.forEach(f => {
-      let props = require(`./komutlar/${f}`);
-      log(`Yüklenen komut: ${props.help.name}.`);
-      client.commands.set(props.help.name, props);
-      props.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, props.help.name);
-      });
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
     });
   });
-  client.reload = command => {
-    return new Promise((resolve, reject) => {
-      try {
-        delete require.cache[require.resolve(`./komutlar/${command}`)];
-        let cmd = require(`./komutlar/${command}`);
-        client.commands.delete(command);
-        client.aliases.forEach((cmd, alias) => {
-          if (cmd === command) client.aliases.delete(alias);
-        });
-        client.commands.set(command, cmd);
-        cmd.conf.aliases.forEach(alias => {
-          client.aliases.set(alias, cmd.help.name);
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    });
-  };
-  client.load = command => {
-    return new Promise((resolve, reject) => {
-      try {
-        let cmd = require(`./komutlar/${command}`);
-        client.commands.set(command, cmd);
-        cmd.conf.aliases.forEach(alias => {
-          client.aliases.set(alias, cmd.help.name);
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    });
-  };
-  client.unload = command => {
-    return new Promise((resolve, reject) => {
-      try {
-        delete require.cache[require.resolve(`./komutlar/${command}`)];
-        let cmd = require(`./komutlar/${command}`);
-        client.commands.delete(command);
-        client.aliases.forEach((cmd, alias) => {
-          if (cmd === command) client.aliases.delete(alias);
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    });
-  };
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+}
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.botowner) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
 
 
-  client.login(ayarlar.token);
+client.on("ready", () =>{
+  
+  console.log("bot aktif knk")
+  
+})
 
+
+
+
+client.login(process.env.token);
+require('discord-buttons')(client);
+
+
+//AFK SİSTEMİ
+
+
+const ms = require("ms");
+const db = require("quick.db");
+client.on("message" , message => {
+    let betaafk = new Discord.MessageEmbed()
+        .setFooter(`Sanctus Furkan & Sanctus Code`)
+      if(!message.guild) return;
+     if (message.content.includes(`afk`)) return;
+      let etiket = message.mentions.users.first()
+      let uye = db.fetch(`user_${message.author.id}_${message.guild.id}`)
+      let nickk = db.fetch(`nick_${message.author.id}_${message.guild.id}`)
+      if(etiket){
+        let reason = db.fetch(`sebep_${etiket.id}_${message.guild.id}`)
+        let uye2 = db.fetch(`user_${etiket.id}_${message.guild.id}`)
+        if(message.content.includes(uye2)){
+        let time = db.fetch(`afktime_${message.guild.id}`);
+        let timeObj = ms(Date.now() - time);
+          message.channel.send(betaafk.setDescription(`${etiket} adlı kullanıcı **${reason}** sebebiyle \`${timeObj}\` süresi boyunca afk.`).setColor("#2F3136"))}}
+    if(message.author.id === uye){  
+        message.member.setNickname(nickk)
+        db.delete(`sebep_${message.author.id}_${message.guild.id}`)
+        db.delete(`user_${message.author.id}_${message.guild.id}`)
+        db.delete(`nick_${message.author.id}_${message.guild.id}`)
+        db.delete(`user_${message.author.id}_${message.guild.id}`);
+        db.delete(`afktime_${message.guild.id}`)
+        message.reply(`**Başarıyla \`AFK\` modundan çıkış yaptın.**`)
+      }  
+    })
+///Sanctus Code
+
+
+
+
+//MOD LOG 
+
+
+const botadi = "SANCTUS CODE"
+
+client.on('guildBanAdd', async (guild, user) => {
+  let modlogs = db.get(`modlogkanaly_${guild.id}`)
+  const modlogkanal = guild.channels.cache.find(kanal => kanal.id === modlogs);
+  if(!modlogs) return;
+  if(modlogs) {
+    let embed = new Discord.MessageEmbed()
+    .setColor("#fffa00")
+    .setAuthor("Bir kişi sunucudan yasaklandı")
+    .setThumbnail(user.avatarURL()||user.defaultAvatarURL)
+    .addField(`Yasaklanan kişi`, `\`\`\` ${user.tag} \`\`\` `)
+    .setFooter(`${botadi} | Mod-Log Sistemi`)
+    .setTimestamp()
+    modlogkanal.send(embed)
+  }
+});
+
+
+client.on('guildBanRemove', async (guild, user) => {
+  let modlogs = db.get(`modlogkanaly_${guild.id}`)
+   const modlogkanal = guild.channels.cache.find(kanal => kanal.id === modlogs);
+   if(!modlogs) return;
+   if(modlogs) {
+     let embed = new Discord.MessageEmbed()
+     .setColor("#fffa00")
+     .setAuthor("Bir kişinin yasağı kaldırıldı")
+     .setThumbnail(user.avatarURL()||user.defaultAvatarURL)
+     .addField(`Yasağı kaldırılan kişi`, `\`\`\` ${user.tag} \`\`\` `)
+     .setFooter(`${botadi} | Mod-Log Sistemi`)
+     .setTimestamp()
+     modlogkanal.send(embed)
+   }
+ });
+
+
+ client.on('channelCreate', async channel => {
+  let modlogs = db.get(`modlogkanaly_${channel.guild.id}`)
+  let entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_CREATE'}).then(audit => audit.entries.first())
+  let user = client.users.cache.get(entry.executor.id)
+   const modlogkanal = channel.guild.channels.cache.find(kanal => kanal.id === modlogs);
+   if(!modlogs) return;
+   if(modlogs) {
+     if (channel.type === "text") {
+       let embed = new Discord.MessageEmbed()
+       .setColor("#fffa00")
+       .setAuthor("Bir Kanal Oluşturuldu")
+       .addField(`Oluşturulan Kanalın İsmi : `, `${channel.name}`)
+       .addField(`Oluşturulan Kanalın Türü : `, `Yazı`)
+       .addField(`Kanalı Oluşturan : `, `<@${user.id}>`)
+       .setFooter(`${botadi} | Mod-Log Sistemi`)
+       .setTimestamp()
+       modlogkanal.send(embed)
+     }
+       if (channel.type === "voice") {
+       
+         let embed = new Discord.MessageEmbed()
+         .setColor("#fffa00")
+         .setAuthor("Bir Kanal Oluşturuldu")
+         .addField(`Oluşturulan Kanalın İsmi : `, `${channel.name}`)
+         .addField(`Oluşturulan Kanalın Türü : `, `Ses`)
+         .addField(`Kanalı Oluşturan : `, `<@${user.id}>`)
+         .setFooter(`${botadi} | Mod-Log Sistemi`)
+         .setTimestamp()
+         modlogkanal.send(embed)
+ 
+ 
+     }
+ }});
+
+ client.on('channelDelete', async channel => {
+  let entry = await channel.guild.fetchAuditLogs({type: 'CHANNEL_DELETE'}).then(audit => audit.entries.first())
+let user = client.users.cache.get(entry.executor.id)
+let modlogs = db.get(`modlogkanaly_${channel.guild.id}`)
+const modlogkanal = channel.guild.channels.cache.find(kanal => kanal.id === modlogs);
+if(!modlogs) return;
+if(modlogs) {
+if (channel.type === "text") {
+let embed = new Discord.MessageEmbed()
+.setColor("#fffa00")
+.setAuthor("Bir Kanal Silindi")
+.addField(`Silinen Kanalın İsmi : `, `${channel.name}`)
+.addField(`Silinen Kanalın Türü : `, `Yazı`)
+.addField(`Kanalı Silen : `, `<@${user.id}>`)
+.setFooter(`${botadi} | Mod-Log Sistemi`)
+.setTimestamp()
+modlogkanal.send(embed)
+}
+  if (channel.type === "voice") {
+
+    let embed = new Discord.MessageEmbed()
+    .setColor("#fffa00")
+    .setAuthor("Bir Kanal Silindi")
+    .addField(`Silinen Kanalın İsmi : `, `${channel.name}`)
+    .addField(`Silinen Kanalın Türü : `, `Ses`)
+    .addField(`Kanalı Silen : `, `<@${user.id}>`)
+    .setFooter(`${botadi} | Mod-Log Sistemi`)
+    .setTimestamp()
+    modlogkanal.send(embed)
+   }
+  }
+});
+
+client.on('roleDelete', async role => {
+  let modlogs =  db.get(`modlogkanaly_${role.guild.id}`)
+   let entry = await role.guild.fetchAuditLogs({type: 'ROLE_DELETE'}).then(audit => audit.entries.first())
+   let user = client.users.cache.get(entry.executor.id)
+  const modlogkanal = role.guild.channels.cache.find(kanal => kanal.id === modlogs);
+   if(!modlogs) return;
+   if(modlogs) {
+     let embed = new Discord.MessageEmbed()
+     .setColor("#fffa00")
+     .setAuthor("Bir Rol Silindi")
+     .addField(`Silinen Rolün İsmi : `, `${role.name}`)
+     .addField(`Rolü Silen : `, `<@${user.id}>`)
+     .setFooter(`${botadi} | Mod-Log Sistemi`)
+     .setTimestamp()
+     modlogkanal.send(embed)
+   }
+ });
+ 
+ client.on('emojiDelete', async emoji => {
+  let modlogs = db.get(`modlogkanaly_${emoji.guild.id}`)
+  let entry = await emoji.guild.fetchAuditLogs({type: 'EMOJI_DELETE'}).then(audit => audit.entries.first())
+  let user = client.users.cache.get(entry.executor.id)
+   const modlogkanal = emoji.guild.channels.cache.find(kanal => kanal.id === modlogs);
+   if(!modlogs) return;
+   if(modlogs) {
+     let embed = new Discord.MessageEmbed()
+     .setColor("#fffa00")
+     .setAuthor("Bir Emoji Silindi")
+     .addField(`Silinen Emojinin İsmi : `, `${emoji.name}`)
+     .addField(`Emojiyi Silen : `, `<@${user.id}>`)
+     .setFooter(`${botadi} | Mod-Log Sistemi`)
+     .setTimestamp()
+     modlogkanal.send(embed)
+   }
+ });
+  
+
+ client.on('roleCreate', async role => {
+  let modlogs =  db.get(`modlogkanaly_${role.guild.id}`)
+  let entry = await role.guild.fetchAuditLogs({type: 'ROLE_CREATE'}).then(audit => audit.entries.first())
+  let user = client.users.cache.get(entry.executor.id)
+    const modlogkanal = role.guild.channels.cache.find(kanal => kanal.id === modlogs);
+    if(!modlogs) return;
+    if(modlogs) {
+      let embed = new Discord.MessageEmbed()
+      .setColor("#fffa00")
+      .setAuthor("Bir Rol Oluşturuldu")
+      .addField(`Oluşturulan Rolün İsmi : `, `${role.name}`)
+      .addField(`Rolü Oluşturan : `, `<@${user.id}>`)
+      .setFooter(`${botadi} | Mod-Log Sistemi`)
+      .setTimestamp()
+      modlogkanal.send(embed)
+    }
+  });
+  
+  
+  client.on('emojiCreate', async emoji => {
+   let modlogs = db.get(`modlogkanaly_${emoji.guild.id}`)
+   let entry = await role.guild.fetchAuditLogs({type: 'EMOJI_CREATE'}).then(audit => audit.entries.first())
+   let user = client.users.cache.get(entry.executor.id)
+    const modlogkanal = emoji.guild.channels.cache.find(kanal => kanal.id === modlogs);
+    if(!modlogs) return;
+    if(modlogs) {
+      let embed = new Discord.MessageEmbed()
+      .setColor("#fffa00")
+      .setAuthor("Bir Emoji Oluşturuldu")
+      .addField(`Oluşturulan Emojinin İsmi : `, `${emoji.name}`)
+      .addField(`Emoji Silen : `, `<@${user.id}>`)
+      .setFooter(`${botadi} | Mod-Log Sistemi`)
+      .setTimestamp()
+      modlogkanal.send(embed)
+    }
+  });
+
+//MESAJ LOG
+client.on("messageUpdate", async (oldMessage, newMessage) => {
+  if (newMessage.author.bot || newMessage.channel.type === "dm") return;
+  if (newMessage.content.startsWith(prefix)) return;
+  let sc = await db.fetch(`modlogkanaly_${newMessage.guild.id}`);
+  let scbul = newMessage.guild.channels.cache.get(sc)
+  if(!scbul) {
+    
+  }
+  if (oldMessage.content == newMessage.content) return;
+  let embed = new Discord.MessageEmbed()
+    .setColor("#fffa00")
+    .setAuthor(`Mesaj Düzenlendi`, newMessage.author.avatarURL())
+    .addField("Kullanıcı", newMessage.author)
+    .addField("Eski Mesaj", "```" + oldMessage.content + "```")
+    .addField("Yeni Mesaj", "```" + newMessage.content + "```")
+    .addField("Kanal Adı", newMessage.channel.name)
+    .addField("Mesaj ID", newMessage.id)
+    .addField("Kullanıcı ID", newMessage.author.id)
+    .setFooter(`Bilgilendirme  • bügün saat ${newMessage.createdAt.getHours() +
+        3}:${newMessage.createdAt.getMinutes()}`
+    );
+  scbul.send(embed);
+});
+
+client.on("messageDelete", async deletedMessage => {
+  if (deletedMessage.author.bot || deletedMessage.channel.type === "dm") return;
+  if (deletedMessage.content.startsWith(prefix)) return;
+  let sc = await db.fetch(`modlogkanaly_${deletedMessage.guild.id}`);
+  let scbul = deletedMessage.guild.channels.cache.get(sc)
+  if(!scbul) {
+    
+  }
+  let embed = new Discord.MessageEmbed()
+    .setColor("#fffa00")
+    .setAuthor(`Mesaj Silindi`, deletedMessage.author.avatarURL())
+    .addField("Kullanıcı", deletedMessage.author)
+    .addField("Silinen Mesaj", "```" + deletedMessage.content + "```")
+    .addField("Kanal Adı", deletedMessage.channel.name)
+    .addField("Mesaj ID", deletedMessage.id)
+    .addField("Kullanıcı ID", deletedMessage.author.id)
+    .setFooter(`Bilgilendirme  • bügün saat ${deletedMessage.createdAt.getHours() +
+        3}:${deletedMessage.createdAt.getMinutes()}`
+    );
+  scbul.send(embed);
+});
+
+
+//Sese Sokma
+
+client.on("ready", () => {
+	client.channels.cache.get(ayarlar.sesli).join();});
+
+
+
+//Otorol
+
+
+client.on("guilMemberAdd", member => {
+  member.roles.add(ayarlar.kayıtsız);
+  
+
+});
+
+
+
+
+
+  
+  
