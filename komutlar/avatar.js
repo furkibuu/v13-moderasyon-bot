fe530e77c261a78b367144cdc6cff23c660a25b4
Sanctus Furkan <74312970+fu-w@users.noreply.github.com>
2021-07-16 00:41:32 +0300

    Create avatar.js

---
+++ komutlar/avatar.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const { MessageButton } = require('discord-buttons');
+
+exports.run = async (client, message, args) => {
+
+  if(message.mentions.users.first()) message.author = message.mentions.users.first();
+
+  const embed = new Discord.MessageEmbed()
+  .setImage(message.author.displayAvatarURL({ dynamic: true, size: 4096, format: 'png' }))
+  .setColor('GOLD');
+
+  const button = new MessageButton()
+  .setLabel('Avatar URL')
+  .setStyle('url')
+  .setURL(message.author.displayAvatarURL({ dynamic: true, size: 4096, format: 'png' }));
+
+  return message.channel.send({ embed: embed, component: button });
+
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+exports.help = {
+  name: 'avatar',
+  description: 'Sen anan',
+  usage: 'avatar'
+};
